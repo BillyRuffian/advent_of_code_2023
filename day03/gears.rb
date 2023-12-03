85517a39a6e53831a5eb099250b55798ba14bfe7
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2023-12-03 22:36:47 +0000

    part 2

--- day03/gears.rb
+++ day03/gears.rb
@@ -28,4 +28,42 @@ def part1
   }.compact.sum
 end
 
+NumberLocation = Data.define(:number, :location, :symbol_location)
+
+
+def part2
+
+  lines = File.readlines('input.txt', chomp: true)
+  number_loc = {}
+  symbol_loc = {}
+
+  lines.each_with_index do |line, line_idx|
+    number_loc.merge! line.enum_for(:scan, /([0-9]+)/).map { [[line_idx, Regexp.last_match.begin(0)], Regexp.last_match[1]] }.to_h
+    symbol_loc.merge! line.enum_for(:scan, /(\*)/).map { [[line_idx, Regexp.last_match.begin(0)], Regexp.last_match[1]] }.to_h
+  end
+
+
+  potenitals = number_loc.each.with_object([]) { |(location, number), memo|
+    line, col = location 
+    length = number.chars.count
+
+    locations = (line-1..line+1).each { |search_line|
+      (col-1..col+length).each { |search_col|
+        memo << NumberLocation.new(number.to_i, [line, col], [search_line, search_col]) if symbol_loc[[search_line, search_col]]
+      }
+    }
+    memo
+  }
+
+  matches = potenitals.combination(2).with_object([]) do |(first, second), memo|
+    memo << first.number * second.number if first.symbol_location == second.symbol_location
+    memo
+  end
+
+  pp matches.sum
+    
+end
+
+
 part1
+part2
