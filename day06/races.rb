594dcf7800739dbc628407aa3bb5b24660714a96
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2023-12-06 08:41:54 +0000

    comment line banishment

--- day06/races.rb
+++ day06/races.rb
@@ -6,8 +6,6 @@ lines = File.readlines('input.txt', chomp: true)
 
 times_and_distances = lines.map { |l| l.split.drop(1).map(&:to_i) }.then { |(t,d)| t.zip(d) }
 
-# pp (1..7).map { |t| t * (7 - t) > 9 ? 1 : 0}.sum
-
 ways_to_win = -> (time, distance) { (1..time).map {|t| t * (time - t) > distance ? 1 : 0 }.sum }
 
 pp times_and_distances.map { |(t, d)| ways_to_win.(t,d) }.inject(&:*)
